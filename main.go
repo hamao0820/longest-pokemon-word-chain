c171bdebafbf4054d5b09f4a0c268e3427a55c04
浜田　俊祐 <hamashun0820@icloud.com>
2024-03-11 12:23:25 +0900

    add: longest

---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+)
+
+//go:embed data/words.json
+var wordsJSON []byte
+
+type Pokemon struct {
+	No   int    `json:"no"`
+	Name string `json:"name"`
+}
+
+type Word struct {
+	Pokemon
+	Ruby   string `json:"reading"`
+	Start  string `json:"start"`
+	End    string `json:"end"`
+	IsLast bool   `json:"is_last"`
+}
+
+var words []Word
+var noMap map[int]Word = make(map[int]Word)
+var startMap map[string][]Word = make(map[string][]Word)
+
+func init() {
+	json.Unmarshal(wordsJSON, &words)
+
+	for _, w := range words {
+		noMap[w.No] = w
+		startMap[w.Start] = append(startMap[w.Start], w)
+	}
+}
+
+type Edge struct {
+	From int
+	To   int
+}
+
+func main() {
+	li := map[int][]Word{} // 隣接リスト
+	for i := 0; i < len(words); i++ {
+		w := words[i]
+		if !isFistGeneration(&w.Pokemon) {
+			continue
+		}
+		for _, v := range startMap[w.End] {
+			if !isFistGeneration(&v.Pokemon) {
+				continue
+			}
+			li[w.No] = append(li[w.No], v)
+		}
+	}
+
+	getLongestPath := func(start int) ([]int, uint64) {
+		visited := make(map[int]bool)
+		path := []int{}
+		longestPath := []int{}
+		maxLength := 0
+		var count uint64
+		// 最長単純路を求める
+		var backtrack func(int) bool
+		backtrack = func(v int) bool {
+			count++
+			visited[v] = true
+			path = append(path, v)
+			if noMap[v].IsLast && len(path) > maxLength {
+				maxLength = len(path)
+				longestPath = append([]int{}, path...)
+			}
+			for _, u := range li[v] {
+				if visited[u.No] {
+					continue
+				}
+				backtrack(u.No)
+			}
+			path = path[:len(path)-1]
+			visited[v] = false
+			return false
+		}
+		backtrack(start)
+		return longestPath, count
+	}
+
+	type result struct {
+		Start  int    `json:"start"`
+		Path   []Word `json:"chain_words"`
+		Length int    `json:"length"`
+	}
+	results := []result{}
+	f := func(i int, wg *sync.WaitGroup) {
+		defer wg.Done()
+		s := time.Now()
+		p, _ := getLongestPath(i)
+		chainWords := []Word{}
+		for _, v := range p {
+			chainWords = append(chainWords, noMap[v])
+		}
+		results = append(results, result{i, chainWords, len(p)})
+		fmt.Printf("start: %d; time: %v\n", i, time.Since(s))
+	}
+	var wg sync.WaitGroup
+	for i := 1; i <= 151; i++ {
+		wg.Add(1)
+		go f(i, &wg)
+	}
+	wg.Wait()
+	fmt.Println("done")
+
+	// results to JSON
+	resultsJSON, _ := json.Marshal(results)
+	resultFile, _ := os.Create("data/results.json")
+	defer resultFile.Close()
+	resultFile.Write(resultsJSON)
+}
+
+func isFistGeneration(p *Pokemon) bool {
+	return p.No <= 151
+}
---
+++ preprocess/preprocess.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+type Pokemon struct {
+	No   int    `json:"no"`
+	Name string `json:"name"`
+}
+
+type Data struct {
+	Data []Pokemon `json:"data"`
+}
+
+type Word struct {
+	Pokemon
+	Ruby   string `json:"reading"`
+	Start  string `json:"start"`
+	End    string `json:"end"`
+	IsLast bool   `json:"is_last"`
+}
+
+type SmallKana string
+
+const (
+	SmallYa  SmallKana = "ャ"
+	SmallYu  SmallKana = "ュ"
+	SmallYo  SmallKana = "ョ"
+	SmallA   SmallKana = "ァ"
+	SmallI   SmallKana = "ィ"
+	SmallU   SmallKana = "ゥ"
+	SmallE   SmallKana = "ェ"
+	SmallO   SmallKana = "ォ"
+	SmallTsu SmallKana = "ッ"
+)
+
+var largeKanaMap = map[SmallKana]string{}
+
+func init() {
+	largeKanaMap[SmallYa] = "ヤ"
+	largeKanaMap[SmallYu] = "ユ"
+	largeKanaMap[SmallYo] = "ヨ"
+	largeKanaMap[SmallA] = "ア"
+	largeKanaMap[SmallI] = "イ"
+	largeKanaMap[SmallU] = "ウ"
+	largeKanaMap[SmallE] = "エ"
+	largeKanaMap[SmallO] = "オ"
+	largeKanaMap[SmallTsu] = "ツ"
+}
+
+func main() {
+	f, err := os.Open("data/zukan.json")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	var data Data
+	json.NewDecoder(f).Decode(&data)
+
+	words := []Word{}
+	for _, p := range data.Data {
+		var w Word
+		w.Pokemon = p
+
+		var ruby string
+		switch p.Name {
+		case "ニドラン♂":
+			ruby = "ニドラオス"
+		case "ニドラン♀":
+			ruby = "ニドランメス"
+		case "ポリゴン2":
+			ruby = "ポリゴンツー"
+		case "ポリゴンZ":
+			ruby = "ポリゴンゼット"
+		default:
+			ruby = p.Name
+		}
+		w.Ruby = ruby
+
+		r := []rune(w.Ruby)
+		w.Start = string(r[:1])
+		endIndex := len(r) - 1
+		end := r[endIndex : endIndex+1]
+
+		pattern := `[アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワンヴガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポ]`
+		re, err := regexp.Compile(pattern)
+		if err != nil {
+			panic(err)
+		}
+
+		// ルールは(https://pokemon-shiritori.com/howto)に準拠
+		// 伸ばし棒で終わる場合
+		if string(end) == "ー" {
+			endIndex--
+			end = r[endIndex : endIndex+1]
+		}
+
+		// 小文字で終わる場合
+		if l, ok := largeKanaMap[SmallKana(end)]; ok {
+			end = []rune(l)
+		}
+
+		w.End = string(end)
+
+		if len(w.End) == 0 {
+			fmt.Println(p.Name)
+			panic("end is empty")
+		}
+
+		if w.End == "ン" {
+			w.IsLast = true
+		}
+
+		if !re.MatchString(w.End) {
+			fmt.Println(p.Name)
+			panic("不正な文字が含まれています")
+		}
+
+		words = append(words, w)
+	}
+
+	j, err := json.Marshal(words)
+	if err != nil {
+		panic(err)
+	}
+
+	f, err = os.Create("data/words.json")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	_, err = f.Write(j)
+	if err != nil {
+		panic(err)
+	}
+}
